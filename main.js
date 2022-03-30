c5cd1b8fcf8728ccb1eaf0d8cab720a06bd16ce2
msb741852 <msb741852@naver.com>
2022-03-30 13:25:56 +0900

    flex, @media를 사용한 반응형 헤더

---
+++ main.js
@@ -0,0 +1,8 @@
+const toggleBtn = document.querySelector('.toggleBtn');
+const menu = document.querySelector('.navbar_menu');
+const icons = document.querySelector('.icons');
+
+toggleBtn.addEventListener('click', ()=> {
+    menu.classList.toggle('active');
+    icons.classList.toggle('active');
+})
