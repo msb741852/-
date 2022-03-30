fdc31a7e4e65fbf6f085d2477d1e72a3b70acd83
msb741852 <msb741852@naver.com>
2022-03-30 16:23:43 +0900

    transition update

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 const toggleBtn = document.querySelector('.toggleBtn');
 const menu = document.querySelector('.navbar_menu');
+const navbar = document.querySelector('.navbar');
 const icons = document.querySelector('.icons');
 
 toggleBtn.addEventListener('click', ()=> {
     menu.classList.toggle('active');
     icons.classList.toggle('active');
+    navbar.classList.toggle('active');
 })
